5749a7454864198cf01abb429167a34d2ed2a6a1
mpottebaum <mpottebaum@gmail.com>
2020-06-10 14:54:20 -0500

    styled results info and page navs

--- components/Results.js
+++ components/Results.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { StyleSheet, View, Text, FlatList, TouchableOpacity } from 'react-native'
+import { StyleSheet, View, Text, FlatList, TouchableOpacity, SafeAreaView, Dimensions } from 'react-native'
 import Result from './Result'
 import { getResults } from '../actions/results'
 
@@ -11,8 +11,12 @@ class Results extends React.Component {
         return Math.ceil(results.totalItems / results.itemsPerPage)
     }
 
-    handlePress = () => {
+    handleNextPress = () => {
         this.props.getResults(this.props.query, (this.props.page + 1))
+    }
+
+    handlePrevPress = () => {
+        this.props.getResults(this.props.query, (this.props.page - 1))
     }
 
     render() {
@@ -23,16 +27,35 @@ class Results extends React.Component {
                 <Text>Loading</Text>
                 :
                 <View style={styles.container}>
-                    <Text>{this.props.results.totalItems} results</Text>
-                    <Text>Showing page {this.props.page} of {this.numPages()}</Text>
-                    <TouchableOpacity onPress={this.handlePress}>
-                        <Text>Next Page</Text>
-                    </TouchableOpacity>
-                    <FlatList
-                        data={items}
-                        renderItem={({ item }) => <Result result={item} />}
-                        keyExtractor={result => result.id}
-                    />
+                    <View style={styles.resultsBar}>
+                        <View style={styles.column}>
+                            {
+                                this.props.page > 1 ?
+                                <TouchableOpacity onPress={this.handlePrevPress} style={styles.pageNav}>
+                                    <Text style={styles.info}>Previous Page</Text>
+                                </TouchableOpacity>
+                                :
+                                null
+                            }
+                        </View>
+                        <View style={styles.column}>
+                            <Text style={styles.info}>{this.props.results.totalItems} results</Text>
+                            <Text style={styles.info}>Page {this.props.page} of {this.numPages()}</Text>
+                        </View>
+                        <View style={styles.column}>
+                            <TouchableOpacity onPress={this.handleNextPress} style={styles.pageNav}>
+                                <Text style={styles.info}>Next Page</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                    <SafeAreaView style={styles.results}>
+                        <FlatList
+                            data={items}
+                            renderItem={({ item }) => <Result result={item} />}
+                            keyExtractor={result => result.id}
+                            // contentContainerStyle={styles.results}
+                        />
+                    </SafeAreaView>
                 </View>
 
             }
@@ -60,5 +83,31 @@ export default connect(mapStateToProps, mapDispatchToProps)(Results)
 const styles = StyleSheet.create({
     container: {
         flex: 1
+    },
+    resultsBar: {
+        flex: 1,
+        flexDirection: 'row'
+    },
+    column: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center'
+    },
+    info: {
+        textAlign: 'center'
+    },
+    pageNav: {
+        width: Dimensions.get('window').width / 3,
+        padding: 20,
+        backgroundColor: '#a9b6c9'
+    },
+    prev: {
+
+    },
+    next: {
+
+    },
+    results: {
+        flex: 8
     }
 })
